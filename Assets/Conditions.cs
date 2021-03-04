6a1025319ebc2891d7fa0b3f77f0ebb7e48b20de
Don_Denki <dantlerdavid@gmail.com>
2021-03-04 11:57:30 +0100

    All Conditions

--- Assets/Conditions.cs
+++ Assets/Conditions.cs
@@ -4,15 +4,90 @@ using UnityEngine;
 
 public class Conditions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool SpacePressed = false;
+    private float time;
+
+    private bool spacePressed;
+    private bool buttonPressed;
+    private bool fourGreaterThanFive;
+    private bool numbersPressed;
+
+    private bool keyOne;
+    private bool keyTwo;
+    private bool keyThree;
+    private bool keyFour;
+
+    public void Update()
     {
-        
+        if ((spacePressed && buttonPressed) || (buttonPressed && fourGreaterThanFive) || (fourGreaterThanFive && numbersPressed))
+        {
+            Debug.Log("true");
+        }
+        else
+        {
+            Debug.Log("false");
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            time += Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            time = 0;
+        }
+        if (time > 3)
+        {
+            spacePressed = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            keyOne = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            keyTwo = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            keyThree = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            keyFour = true;
+        }
+
+        if (keyOne && keyTwo && keyThree && keyFour)
+        {
+            numbersPressed = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ButtonPressed()
     {
-        
+        buttonPressed = true;
+    }
+
+    public void GenerateNumbers()
+    {
+        int numGreaterThanFive = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int number = Random.Range(1, 11);
+            if (number > 5)
+            {
+                numGreaterThanFive++;
+            }
+        }
+
+        if (numGreaterThanFive >=4)
+        {
+            fourGreaterThanFive = true;
+        }
     }
 }
